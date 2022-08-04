c9f0408b60acc3a6f20dc33abfd38f44ff1938a4
HongBin2112 <benson890720@gmail.com>
2022-08-04 20:29:20 +0800

    new func: click to show info

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
 
 
-var test_var = "hello from the indec.js"
 
 
-console.log(test_var)
+function click_change_display(element_id){
+    var x = document.getElementById(element_id);
 
+    /*
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        x.style.opacity = "1";
+      } else {
+        x.style.display = "none";
+        x.style.opacity = "0.3";
+      }
+      */
+
+    if (x.style.visibility === "visible") {
+      x.style.visibility = "hidden";
+      //x.style.display = "none";
+      x.style.opacity = "0.1";
+    } else {
+      x.style.visibility = "visible";
+      x.style.display = "block";
+      x.style.opacity = "1";
+    }    
+}
+
+
+function card_click_show_info(card_id){
+    
+}
+
+
+
+
+/* DEBUG */
 
