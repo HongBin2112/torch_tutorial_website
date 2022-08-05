ff4ad103fe195333bc4b2f87614d68b0f0758c63
HongBin2112 <benson890720@gmail.com>
2022-08-05 17:58:22 +0800

    smoothly expand

--- js/index.js
+++ js/index.js
@@ -1,38 +1,86 @@
 
+const cards_id = ["part1_card", "part2_card", "part3_card"];
+const contents_id = ["part1_content", "part2_content", "part3_content"];
+
+
+var dict_cards_id = {};
+for(var i = 0; i < cards_id.length; i++){
+  dict_cards_id[cards_id[i]] = contents_id[i];
+
+  document.getElementById(cards_id[i]).onclick = card_click_show_info;
+}
+
+
+
 
 
 
 function click_change_display(element_id){
     var x = document.getElementById(element_id);
 
-    /*
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        x.style.opacity = "1";
-      } else {
-        x.style.display = "none";
-        x.style.opacity = "0.3";
-      }
-      */
-
     if (x.style.visibility === "visible") {
-      x.style.visibility = "hidden";
       //x.style.display = "none";
-      x.style.opacity = "0.1";
+      x.style.visibility = "hidden";
+      x.style.opacity = "0";
+      x.style.maxHeight = null;
+      
     } else {
-      x.style.visibility = "visible";
       x.style.display = "block";
+      x.style.visibility = "visible";
       x.style.opacity = "1";
+      //this is the keypoint: smooth expand the content.
+      x.style.maxHeight = x.scrollHeight + "px"; 
     }    
 }
 
 
-function card_click_show_info(card_id){
+
+function card_click_show_info(){
+  var card_id = this.id;
+  var content_id = dict_cards_id[card_id];
+
+  for(i = 0; i < cards_id.length; i++){
+    var i_content_id = dict_cards_id[cards_id[i]];
+    var i_content_element = document.getElementById(i_content_id);
     
+    if(i_content_id != content_id){
+      i_content_element.style.maxHeight = null;
+      i_content_element.style.display = "none";
+      i_content_element.style.visibility = "hidden";
+  
+    } else {
+      click_change_display(content_id);
+      
+    }
+
+  }
+
+  //after info shows, click anchor<a> under content_title_box to move.
+  var info_title_anchor = document.getElementById(content_id).getElementsByTagName("a")[0];
+  setTimeout(function() {
+    info_title_anchor.click();
+  }, 260);
+
+
 }
+
+
+
+
+
+
+
+
 
 
 
 
 /* DEBUG */
+var debug_mode = 0;
+
+if(debug_mode == 1){
+  console.log("debug...");
+
+  
+}
 
